c25d70bbbbd6b0e88dc009d97322c2d12ca867df
Lmovingon <lmovingon2014@gmail.com>
2015-04-15 11:39:38 +0800

    2015/04/15

--- nodejs/server.js
+++ nodejs/server.js
@@ -4,13 +4,21 @@ var url = require('url');
 function start(route, handle) {
     
     http.createServer(function(request, response) {
-        var pathname = url.parse(request.url).pathname;
-        console.log(pathname);
         
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        var content = route(handle, pathname);
-        response.write(content);
-        response.end();
+        var postData = '';
+        var pathname = url.parse(request.url).pathname;
+        
+        request.setEncoding('utf8');
+        
+        request.addListener('data', function(postDataChunk) {
+            postData += postDataChunk;
+            console.log('received POST data chunk "' + postDataChunk + '".')
+        });
+        request.addListener('end', function() { 
+            route(handle, pathname, response, postData);
+        });
+        
+        
     }).listen(8888);
     
 }
