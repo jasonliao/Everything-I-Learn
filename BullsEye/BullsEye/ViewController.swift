8d33a2896aa4be97ffafc1ce8cd49bdd2b42256d
jasonliao <lmovingon2014@gmail.com>
2017-02-04 21:33:38 +0800

    learn swift

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by  jasonliao on 03/02/2017.
+//  Copyright © 2017  jasonliao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startNewRound()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func showAlert(_ sender: UIButton) {
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        
+        score += points
+        
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+        } else if difference < 5 {
+            title = "You almost had it!"
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "New Round!", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        startNewRound()
+    }
+    
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        currentValue = lroundf(sender.value)
+    }
+    
+    private func startNewRound() {
+        round += 1
+        targetValue = Int(arc4random_uniform(100)) + 1
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    private func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+}
+
---
+++ BullsEye/README.md
@@ -0,0 +1,6 @@
+# What I Can Learn From BullsEys
+
+如何设置应用在启动时就横屏，在 XCode 里选择该项目，在 General 的 Deployment Info 这一栏下，把 Portrait 去掉勾选。
+
+`arc4random_uniform` 方法可以生成随机数，例如 `arc4random_uniform(100)` 可以生成 0 ~ 100 但不包含 100 的随机数。
+
